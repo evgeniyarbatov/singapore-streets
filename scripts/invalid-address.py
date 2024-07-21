4b791533bb79a85d688918c81530dea169313c16
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-21 14:41:14 +0700

    Get more street names

--- scripts/invalid-address.py
+++ scripts/invalid-address.py
@@ -12,11 +12,13 @@ def main():
       starts_with_letter = re.match(r'^[A-Z]', line)
       is_block = re.match(r'^Blk', line, re.IGNORECASE)
       contains_punctuation = bool(re.search(r'[;,#()]', line))
+      has_stop_words = bool(re.search(r'^After', line))
       
       if (
         starts_with_letter and 
         not is_block and
-        not contains_punctuation
+        not contains_punctuation and 
+        not has_stop_words
       ):
         print(line)
       else:
