f7d4ed70e94c3aaf8fe6d493dafef3591c794da8
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-26 00:21:29 +0800

    Add few more streets

--- scripts/invalid-address.py
+++ scripts/invalid-address.py
@@ -11,16 +11,18 @@ def main():
       
       starts_with_letter = re.match(r'^[A-Z]', line)
       is_block = re.match(r'^Blk', line, re.IGNORECASE)
-      contains_punctuation = bool(re.search(r'[;,#()]', line))
-      has_stop_words = bool(re.search(r'(^After|^Before|^Entrance)', line))
+      contains_punctuation = bool(re.search(r'[;,:#()]', line))
+      has_stop_words = bool(re.search(r'(^After|^Before|Opposite|^Entrance|Station|Temple|Park$|Playground|Fitness Centre|Market$|Terminal$)', line))
       has_special_characters = bool(re.search(r'@', line))
+      has_invalid_lorongs = bool(re.search(r'^Lorong\s+\d+$', line))
       
       if (
         starts_with_letter and 
         not is_block and
         not contains_punctuation and 
         not has_stop_words and
-        not has_special_characters
+        not has_special_characters and
+        not has_invalid_lorongs
       ):
         print(line)
       else:
